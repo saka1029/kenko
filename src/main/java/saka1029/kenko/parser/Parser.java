39b16c7eed487637559ca1a1a772aed0fe9378b4
saka1029 <saka1029@gmail.com>
2026-02-11 05:47:20 +0900

    ..

--- src/main/java/saka1029/kenko/parser/Parser.java
+++ src/main/java/saka1029/kenko/parser/Parser.java
@@ -1,6 +1,7 @@
 package saka1029.kenko.parser;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,15 +10,28 @@ public class Parser {
     final Node root;
     final BufferedReader reader;
     final List<Type> types = new ArrayList<>();
-    Node current;
+    Node node;
 
     public Parser(BufferedReader reader) {
-        this.current = this.root = new Node(null, null, null);
+        this.root = new Node(null, null, null);
         this.reader = reader;
+        get();
     }
 
-    Node parse() {
-        return null;
+    Node get() {
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                for (Type type : types) {
+                    Node n = type.createNode(line);
+                    if (n != null)
+                        return node = n;
+                }
+                node.text.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return node = null;
     }
-
 }
