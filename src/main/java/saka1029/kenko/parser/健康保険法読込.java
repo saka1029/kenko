9819ede85465fc84712bd997e59e75f16bae78a5
saka1029 <saka1029@gmail.com>
2026-02-12 03:29:32 +0900

    ..

--- src/main/java/saka1029/kenko/parser/健康保険法読込.java
+++ src/main/java/saka1029/kenko/parser/健康保険法読込.java
@@ -3,13 +3,22 @@ package saka1029.kenko.parser;
 import java.io.BufferedReader;
 import static saka1029.kenko.parser.Pat.*;
 
+/**
+ * SYNTAX
+ * <pre>
+ * 全体     = 注釈 {"章" 条or注釈 {"節" 条or注釈 {"款" 条or注釈}}}
+ * 条or注釈 = {{"条" {{"数字" 漢数字} | 漢数字} | 注釈}
+ * 注釈     = {"注釈"}
+ * 漢数字   = {"漢数字" {"イロハ"}}
+ * </pre>
+ */
 public class 健康保険法読込 extends Parser {
 
     static final Type.IdFunc KAN_ID = n -> "" + Kan2Int(n);
     static final Type.IdFunc KAN_NO_ID = n -> KanNo2Str(n);
     static final Type.IdFunc IROHA_ID = n -> "" + Iroha2Int(n);
     static final Type.IdFunc NUM_ID = n -> Zen2Han(n);
-    public static Type 目次 = new Type("目次", "(?<H>)(?<T>目次)", n -> "#");
+    // public static Type 目次 = new Type("目次", "(?<H>)(?<T>目次)", n -> "#");
     public static Type 章 = new Type("章", "(?<H>第" + P漢数字 + "章)" + P空白 + "(?<T>.*)", KAN_ID);
     public static Type 節 = new Type("節", "(?<H>第" + P漢数字 + "節)" + P空白 + "(?<T>.*)", KAN_ID);
     public static Type 款 = new Type("款", "(?<H>第" + P漢数字 + "款)" + P空白 + "(?<T>.*)", KAN_ID);
@@ -21,7 +30,7 @@ public class 健康保険法読込 extends Parser {
 
     public 健康保険法読込(BufferedReader reader) {
         super(reader);
-        types.add(目次);
+        // types.add(目次);
         types.add(章);
         types.add(節);
         types.add(款);
@@ -32,24 +41,17 @@ public class 健康保険法読込 extends Parser {
         types.add(注釈);
     }
     
-    // void 目次(Node parent) {
-    //     while (eat(章)) {
-    //         Node sho = parent.addChild(eaten);
-    //         while (eat(節)) {
-    //             Node setu = sho.addChild(eaten);
-    //             while (eat(款)) {
-    //                 setu.addChild(eaten);
-    //             }
-    //         }
-    //     }
-    // }
-
     void 漢数字(Node parent) {
         while (eat(漢数字)) {
             Node kans = parent.addChild(eaten);
             while (eat(イロハ))
                 kans.addChild(eaten);
         }
+    }
+
+    void 注釈(Node parent) {
+        while (eat(注釈))
+            parent.addChild(eaten);
     }
 
     void 条or注釈(Node parent) {
@@ -70,8 +72,7 @@ public class 健康保険法読込 extends Parser {
                     }
                 }
             } else if (is(注釈)) {
-                while (eat(注釈))
-                    parent.addChild(eaten);
+                注釈(parent);
             } else
                 break;
         }
@@ -79,8 +80,7 @@ public class 健康保険法読込 extends Parser {
 
     @Override
     void parseMain(Node parent) {
-        while (eat(注釈))
-            parent.addChild(eaten);
+        注釈(parent);
         while (eat(章)) {
             Node syo = parent.addChild(eaten);
             条or注釈(syo);
